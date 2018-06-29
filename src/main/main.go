bf37cf03b5b9c4674e8accc7a706784613cb002e
lasithatharanga <lasitha119@gmail.com>
2018-06-29 17:16:58 +0530

    Insert data executer done

--- src/main/main.go
+++ src/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"main/api/routes"
 	"main/proofs/proofExecuter/stellarexecuter"
@@ -9,8 +10,9 @@ import (
 
 func main() {
 	//create := stellarexecuter.CreateAccount()
-	stellarexecuter.InsertDataHash("", "", "")
+	newRootHash := stellarexecuter.InsertDataHash("E3FC18CB4776193F8AD15A947406DBYE", "SDL26B3CQN4AQHPV3MDRMUB5BXNMCQLHY3HVAD7ZOP4QACX2OL7V2IOW", "001", "cda5c9845b218fdd8e8f04d2db3a82db6b59d1d78785a214cbce3ee13311ee6e")
 	router := routes.NewRouter()
-	//fmt.Println(transfer)
+	fmt.Println("Latest Root hash")
+	fmt.Println(newRootHash)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- src/main/proofs/proofExecuter/stellarexecuter/insertData.go
+++ src/main/proofs/proofExecuter/stellarexecuter/insertData.go
@@ -7,25 +7,14 @@ import (
 	"github.com/stellar/go/clients/horizon"
 )
 
-func InsertDataHash(hash string, secret string, public string) string {
-	source := "SDL26B3CQN4AQHPV3MDRMUB5BXNMCQLHY3HVAD7ZOP4QACX2OL7V2IOW"
-	destination := "GDLTXGZ2XHVDTNGKEXSRSEVNJEJJG6ENOBTE2CILFVWSSFV3ECYZDCTI"
+func InsertDataHash(hash string, secret string, profileId string, rootHash string) string {
 
-	// Make sure destination account exists
-	if _, err := horizon.DefaultTestNetClient.LoadAccount(destination); err != nil {
-		panic(err)
-	}
-
-	//passphrase := network.TestNetworkPassphrase
-
+	// save data
 	tx, err := build.Transaction(
 		build.TestNetwork,
-		build.SourceAccount{source},
+		build.SourceAccount{secret},
 		build.AutoSequence{horizon.DefaultTestNetClient},
-		build.Payment(
-			build.Destination{destination},
-			build.NativeAmount{"10"},
-		),
+		build.SetData(profileId, []byte(hash)),
 	)
 
 	if err != nil {
@@ -33,7 +22,7 @@ func InsertDataHash(hash string, secret string, public string) string {
 	}
 
 	// Sign the transaction to prove you are actually the person sending it.
-	txe, err := tx.Sign(source)
+	txe, err := tx.Sign(secret)
 	if err != nil {
 		panic(err)
 	}
@@ -53,10 +42,51 @@ func InsertDataHash(hash string, secret string, public string) string {
 	fmt.Println("Ledger:", resp.Ledger)
 	fmt.Println("Hash:", resp.Hash)
 
-	return "hellow"
+	return AppendToTree(resp.Hash, rootHash, secret)
 
 }
 
-// func AppendToTree(current string, pre string) string {
+func AppendToTree(current string, pre string, secret string) string {
+	// save data
+	tx, err := build.Transaction()
+	if pre != "" {
+		tx, err = build.Transaction(
+			build.TestNetwork,
+			build.SourceAccount{secret},
+			build.AutoSequence{horizon.DefaultTestNetClient},
+			build.SetData("previous", []byte(pre)),
+			build.SetData("current", []byte(current)),
+		)
+	} else {
+		tx, err = build.Transaction(
+			build.TestNetwork,
+			build.SourceAccount{secret},
+			build.AutoSequence{horizon.DefaultTestNetClient},
+			build.SetData("current", []byte(current)),
+		)
+	}
+	if err != nil {
+		panic(err)
+	}
 
-// }
+	// Sign the transaction to prove you are actually the person sending it.
+	txe, err := tx.Sign(secret)
+	if err != nil {
+		panic(err)
+	}
+
+	txeB64, err := txe.Base64()
+	if err != nil {
+		panic(err)
+	}
+
+	// And finally, send it off to Stellar!
+	resp, err := horizon.DefaultTestNetClient.SubmitTransaction(txeB64)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successful Transaction Tree Appended:")
+	fmt.Println("Ledger:", resp.Ledger)
+	fmt.Println("Hash:", resp.Hash)
+	return resp.Hash
+}
