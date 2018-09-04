37f51646ed9f59084a4d3f078fab0ecf27d49474
Zeemzo <azeemashraf@outlook.com>
2018-09-04 13:09:53 +0530

    poc update for new txn types

--- src/main/proofs/retriever/stellarRetriever/fullPOCRetriever.go
+++ src/main/proofs/retriever/stellarRetriever/fullPOCRetriever.go
@@ -57,7 +57,7 @@ func (db *ConcretePOC) RetrieveFullPOC() model.RetrievePOC {
 			keysBody := out1
 			keys := make([]PublicKeyPOC, 0)
 			json.Unmarshal(keysBody, &keys)
-			
+
 			var bcPreHash string
 			var transactionType string
 			var TDPHash string
@@ -69,63 +69,74 @@ func (db *ConcretePOC) RetrieveFullPOC() model.RetrievePOC {
 			}
 			switch transactionType {
 			case "0":
-				identifier:= Base64DecEnc("Decode", keys[2].Value)
-				temp = model.Current{TXNID: db.Txn, TType: transactionType,Identifier:identifier}
+				identifier := Base64DecEnc("Decode", keys[2].Value)
+				temp = model.Current{TXNID: db.Txn, TType: transactionType, Identifier: identifier}
 			case "1":
-				previousProfile:= Base64DecEnc("Decode", keys[2].Value)
-				identifier:= Base64DecEnc("Decode", keys[3].Value)
+				previousProfile := Base64DecEnc("Decode", keys[2].Value)
+				identifier := Base64DecEnc("Decode", keys[3].Value)
 				temp = model.Current{
-					TXNID: db.Txn, 
-					TType: transactionType,
-					Identifier:identifier,
-					PreviousProfileID:previousProfile}
+					TXNID:             db.Txn,
+					TType:             transactionType,
+					Identifier:        identifier,
+					PreviousProfileID: previousProfile}
 			case "2":
 
-				Profile= Base64DecEnc("Decode", keys[2].Value)
+				Profile = Base64DecEnc("Decode", keys[2].Value)
 				TDPHash = Base64DecEnc("Decode", keys[3].Value)
 
-					fmt.Println("TDPHash")
-					fmt.Println(TDPHash)
-				
+				fmt.Println("TDPHash")
+				fmt.Println(TDPHash)
 
-				temp = model.Current{TXNID: db.Txn, TType: transactionType, DataHash: TDPHash,ProfileID:Profile}
+				temp = model.Current{TXNID: db.Txn, TType: transactionType, DataHash: TDPHash, ProfileID: Profile}
 			case "3":
 			case "4":
 			case "5":
 				temp = model.Current{TXNID: db.Txn, TType: transactionType}
 			case "6":
-				
-					mergeID := Base64DecEnc("Decode", keys[3].Value)
-					Profile= Base64DecEnc("Decode", keys[2].Value)
-					result, err := http.Get("https://horizon-testnet.stellar.org/transactions/" + mergeID + "/operations")
-					if err != nil {
-						Rerr.Code = result.StatusCode
-						Rerr.Message = "The HTTP request failed for Merge ID"
-						response.Txn = db.Txn
-						response.Error = Rerr
-						return response
-					} else {
 
-						if result.StatusCode == 200 {
-							object := ConcretePOC{Txn: mergeID, BCTree: mergeTree, DBTree: db.DBTree, ProfileID: Profile}
-							response = object.RetrieveFullPOC()
+				mergeID := Base64DecEnc("Decode", keys[3].Value)
+				Profile = Base64DecEnc("Decode", keys[2].Value)
+				result, err := http.Get("https://horizon-testnet.stellar.org/transactions/" + mergeID + "/operations")
+				if err != nil {
+					Rerr.Code = result.StatusCode
+					Rerr.Message = "The HTTP request failed for Merge ID"
+					response.Txn = db.Txn
+					response.Error = Rerr
+					return response
+				} else {
 
-							mergeTree = response.BCHash
-						}
+					if result.StatusCode == 200 {
+						object := ConcretePOC{Txn: mergeID, BCTree: mergeTree, DBTree: db.DBTree, ProfileID: Profile}
+						response = object.RetrieveFullPOC()
 
+						mergeTree = response.BCHash
 					}
 
-				
+				}
 
 				temp = model.Current{
-					TXNID: db.Txn, 
-					TType: transactionType, 
-					ProfileID:Profile, 
-					MergedChain: mergeTree, 
-					MergedID: mergeID}
+					TXNID:       db.Txn,
+					TType:       transactionType,
+					ProfileID:   Profile,
+					MergedChain: mergeTree,
+					MergedID:    mergeID}
+			case "7":
 
+				temp = model.Current{
+					TXNID: db.Txn,
+					TType: transactionType}
+			case "8":
 
+				temp = model.Current{
+					TXNID: db.Txn,
+					TType: transactionType}
+			case "9":
+
+				temp = model.Current{
+					TXNID: db.Txn,
+					TType: transactionType}
 			default:
+
 			}
 
 			if keys[1] != (PublicKeyPOC{}) {
@@ -145,9 +156,9 @@ func (db *ConcretePOC) RetrieveFullPOC() model.RetrievePOC {
 
 			if keys[1].Value != "" {
 				object := ConcretePOC{
-					Txn: bcPreHash, 
-					BCTree: db.BCTree, 
-					DBTree: db.DBTree, 
+					Txn:       bcPreHash,
+					BCTree:    db.BCTree,
+					DBTree:    db.DBTree,
 					ProfileID: db.ProfileID}
 				response = object.RetrieveFullPOC()
 			}
