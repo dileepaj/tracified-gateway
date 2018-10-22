a0e5634e041494d194cc23623f1dcbe1c97008ef
jaje <thananjaje3@gmail.com>
2018-10-22 15:47:56 +0530

    Added CORS.

--- src/main/main.go
+++ src/main/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"fmt"
+
 	"log"
 	"main/api/routes"
 	"net/http"
+
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 
+	headersOk := handlers.AllowedHeaders([]string{"Content-Type"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
 	router := routes.NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8030", router))
-
+	log.Fatal(http.ListenAndServe(":8030", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
+	fmt.Println("Gateway Started @port :8030...!")
 }
