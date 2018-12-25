aa6458f2b9d70dfa9b7496659319d6f5dbe0c08e
Zeemzo <azeemashraf@outlook.com>
2018-12-25 16:07:39 +0530

    test

---
+++ api/apiModel/dao.go
@@ -0,0 +1,25 @@
+package apiModel
+
+import(
+	"github.com/dileepaj/tracified-gateway/model"
+	
+)
+
+type GetCOCCollection struct {
+	Collection model.COCCollectionBody
+}
+
+type GetMultiCOCCollection struct {
+	Collection []model.COCCollectionBody
+}
+
+type InsertCOCCollectionResponse struct {
+	Message string
+	Body model.COCCollectionBody
+}
+
+
+type InsertTransactionCollectionResponse struct {
+	Message string
+	Body model.TransactionCollectionBody
+}
