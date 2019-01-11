f11c81e5d0d3f935e171b8d083721d66b3f5dde3
Zeemzo <azeemashraf@outlook.com>
2019-01-11 08:59:29 +0530

    whitewash

--- model/asset.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Asset struct {
-	Code  string `json:"Code"`
-	Limit string `json:"Limit"`
-}
--- model/setOptions.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type SignerKey struct {
-	Publickey string `json:"Publickey"`
-	Weight    string `json:"Weight"`
-}
