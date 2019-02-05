bc30af03dfc29843ac9d517b07b4002c4d0b5164
Zeemzo <azeemashraf@outlook.com>
2019-02-05 11:39:48 +0530

    DB change

--- dao/connection.go
+++ dao/connection.go
@@ -10,8 +10,8 @@ type Connection struct {
 }
 
 func (cd *Connection) connect()(*mgo.Session,error) {
-	// session, err := mgo.Dial("mongodb://Zeemzo:abcd1234@ds143953.mlab.com:43953/tracified-gateway")
-	session, err := mgo.Dial("mongodb://Zeemzo:abcd1234@ds157654.mlab.com:57654/tracified-gateway")
+	session, err := mgo.Dial("mongodb://Zeemzo:abcd1234@ds143953.mlab.com:43953/tracified-gateway")
+	// session, err := mgo.Dial("mongodb://Zeemzo:abcd1234@ds157654.mlab.com:57654/tracified-gateway")
 
 	if err != nil {
 		fmt.Println(err)
