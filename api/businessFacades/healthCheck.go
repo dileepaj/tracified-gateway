21c7ea56cd3ea91a0b3522340e6ef99720b227f1
nisalsp9 <nisalsp9@yahoo.com>
2021-01-07 08:41:18 +0530

    Add health check api code.

--- api/businessFacades/healthCheck.go
+++ api/businessFacades/healthCheck.go
@@ -1,1 +1,25 @@
 package businessFacades
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type HealthCheckResponse struct {
+	Note    string  `json:"note"`
+	Time    string  `json:"time"`
+	Version string `json:"version"`
+}
+
+
+func HealthCheck(w http.ResponseWriter, r *http.Request){
+	resp := HealthCheckResponse{
+		Note: "Gateway up and running",
+		Time:    time.Now().Format("Mon Jan _2 15:04:05 2006"),
+		Version: "Not Found",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+	}
+}
