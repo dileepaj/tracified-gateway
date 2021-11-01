7b921a15ead433ca8c3b510c611d21a55f16ea42
Yasas Wimalarathna <72276384+yasassdk@users.noreply.github.com>
2021-11-01 22:48:43 +0530

    let the env values to be fetched from container level

--- commons/envfilereader.go
+++ commons/envfilereader.go
@@ -12,7 +12,7 @@ func GoDotEnvVariable(key string) string {
 	// load .env file
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		//log.Fatalf("Error loading .env file")
 	}
 	return os.Getenv(key)
 }
