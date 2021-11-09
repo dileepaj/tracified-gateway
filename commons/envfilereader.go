fcaa2289021cffd015133d3e437f0c247a22ce10
Yasas Wimalarathna <72276384+yasassdk@users.noreply.github.com>
2021-11-09 09:35:53 +0530

    Revert "Merge branch 'master' of https://github.com/dileepaj/tracified-gateway"
    This reverts commit d99ea7fdb93531b0c436c961c05298f30affbb8d, reversing
    changes made to f206c7d3691efaa23afc09c92f2612be29d35f60.

--- commons/envfilereader.go
+++ commons/envfilereader.go
@@ -2,6 +2,7 @@ package commons
 
 import (
 	"github.com/joho/godotenv"
+	"log"
 	"os"
 )
 
@@ -11,7 +12,7 @@ func GoDotEnvVariable(key string) string {
 	// load .env file
 	err := godotenv.Load(".env")
 	if err != nil {
-		//log.Fatalf("Error loading .env file")
+		log.Fatalf("Error loading .env file")
 	}
 	return os.Getenv(key)
 }
