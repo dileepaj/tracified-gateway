2b3fcfb7f77d70e1d8ac821771eb4f8e8771df3c
bhawanthas <bpsshanuka@gmail.com>
2022-08-10 16:03:06 +0530

    TRAC-5675 fixed indentation issue

--- pools/sponserAccount.go
+++ pools/sponserAccount.go
@@ -31,10 +31,6 @@ func CreateSponseredAccount(batchAccount model.CoinAccount) (string, string, err
 	logrus.Info("Batch Account PK ", pair.Address())
 	// encrypt key pair and add to DB
 	encSK := commons.Encrypt(pair.Seed())
-	// logrus.Info("keys ----------------------+++++++-------------- ",encPK,"   ",encSK)
-
-	logrus.Info("Encrypted PK ", pair.Address())
-	// logrus.Info("Encrypted SK ", encSK)
 
 	batchAccount.CoinAccountPK = pair.Address()
 	batchAccount.CoinAccountSK = encSK
