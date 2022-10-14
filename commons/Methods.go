65911cd43172d0c96b8fead4fc66607ab51a0612
shanukabps <bpsshanuka@gmail.com>
2022-10-14 11:45:58 -0700

    TRAC-6461 txn build tpdate and remove all other impletetion from master

--- commons/Methods.go
+++ commons/Methods.go
@@ -1,40 +1,9 @@
 package commons
 
-import (
-	"encoding/json"
-	"net/http"
-
-	"github.com/dileepaj/tracified-gateway/model"
-	"github.com/sirupsen/logrus"
-	"github.com/stellar/go/txnbuild"
-)
-
 // work as ternary operator for string
 func ValidateStrings(a, b string) string {
 	if a != "" {
 		return a
 	}
 	return b
-}
-
-func JSONErrorReturn(w http.ResponseWriter, r *http.Request, err string, errorCode int, message string) {
-	logrus.Error(message, " ", err)
-	w.WriteHeader(errorCode)
-	response := model.Error{Code: errorCode, Message: message + " " + err}
-	json.NewEncoder(w).Encode(response)
-	return
-}
-
-func ChunkSlice(slice []txnbuild.Operation, chunkSize int) [][]txnbuild.Operation {
-	var chunks [][]txnbuild.Operation
-	for i := 0; i < len(slice); i += chunkSize {
-		end := i + chunkSize
-		// necessary check to avoid slicing beyond
-		// slice capacity
-		if end > len(slice) {
-			end = len(slice)
-		}
-		chunks = append(chunks, slice[i:end])
-	}
-	return chunks
 }
--- configs/socialImpactemailList.go
+++
@@ -1,3 +0,0 @@
-package configs
-
-var NotificationEmails = [3]string{"shanuka.bps@gmail.com", "thurshah@tracified.com", "ishinik@tracified.com"}
