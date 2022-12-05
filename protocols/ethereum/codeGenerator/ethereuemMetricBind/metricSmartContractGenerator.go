49224f30499b194553dded093486aaec9b7d20a9
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2022-12-05 14:04:27 +0530

    TRAC-6779 ABI nad BIN caller

--- protocols/ethereum/codeGenerator/ethereuemMetricBind/metricSmartContractGenerator.go
+++ protocols/ethereum/codeGenerator/ethereuemMetricBind/metricSmartContractGenerator.go
@@ -10,6 +10,7 @@ import (
 	"github.com/dileepaj/tracified-gateway/commons"
 	"github.com/dileepaj/tracified-gateway/dao"
 	"github.com/dileepaj/tracified-gateway/model"
+	"github.com/dileepaj/tracified-gateway/protocols/ethereum/deploy"
 	"github.com/oklog/ulid"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/text/cases"
@@ -26,6 +27,7 @@ Generate smart contract for metric binding
 func SmartContractGeneratorForMetric(w http.ResponseWriter, r *http.Request, metricBindJson model.MetricDataBindingRequest) {
 	object := dao.Connection{}
 	var status string
+	reqType := "METRIC"
 
 	metricDetails, errWhenGettingMetricStatus := object.GetEthMetricStatus(metricBindJson.Metric.ID).Then(func(data interface{}) interface{} {
 		return data
@@ -98,6 +100,58 @@ func SmartContractGeneratorForMetric(w http.ResponseWriter, r *http.Request, met
 
 		//Write contract template into a file
 		//fo, errInOutput := os.Create(commons.GoDotEnvVariable("METRICCONTRACTLOCATION") + "/" + contractName + `.sol`)
+
+		//call the ABI generator
+		abiString, errWhenGeneratingABI := deploy.GenerateABI(contractName, reqType)
+		if errWhenGeneratingABI != nil {
+			ethMetricObj.Status = "FAILED"
+			ethMetricObj.ErrorMessage = errWhenGeneratingABI.Error()
+			if status == "" {
+				errWhenInsertingMetricToDB := object.InsertToEthMetricDetails(ethMetricObj)
+				if errWhenInsertingMetricToDB != nil {
+					logrus.Error("Error while inserting metric details to the DB " + errWhenInsertingMetricToDB.Error())
+					commons.JSONErrorReturn(w, r, errWhenInsertingMetricToDB.Error(), http.StatusInternalServerError, "Error while inserting metric details to the DB ")
+					return
+				}
+			} else if status == "FAILED" {
+				errWhenUpdatingMetricToDB := object.UpdateEthereumMetricStatus(ethMetricObj.MetricID, ethMetricObj.TransactionUUID, ethMetricObj)
+				if errWhenUpdatingMetricToDB != nil {
+					logrus.Error("Error while updating metric details to the DB " + errWhenUpdatingMetricToDB.Error())
+					commons.JSONErrorReturn(w, r, errWhenUpdatingMetricToDB.Error(), http.StatusInternalServerError, "Error while updating metric details to the DB ")
+					return
+				}
+			}
+			logrus.Info("Error when generating ABI file, ERROR : " + errWhenGeneratingABI.Error())
+			commons.JSONErrorReturn(w, r, errWhenGeneratingABI.Error(), http.StatusInternalServerError, "Error when generating ABI file, ERROR : ")
+			return
+		}
+		ethMetricObj.ABIstring = abiString
+
+		//call the BIN generator
+		binString, errWhenGeneratingBIN := deploy.GenerateBIN(contractName, reqType)
+		if errWhenGeneratingBIN != nil {
+			ethMetricObj.Status = "FAILED"
+			ethMetricObj.ErrorMessage = errWhenGeneratingBIN.Error()
+			if status == "" {
+				errWhenInsertingMetricToDB := object.InsertToEthMetricDetails(ethMetricObj)
+				if errWhenInsertingMetricToDB != nil {
+					logrus.Error("Error while inserting metric details to the DB " + errWhenInsertingMetricToDB.Error())
+					commons.JSONErrorReturn(w, r, errWhenInsertingMetricToDB.Error(), http.StatusInternalServerError, "Error while inserting metric details to the DB ")
+					return
+				}
+			} else if status == "FAILED" {
+				errWhenUpdatingMetricToDB := object.UpdateEthereumMetricStatus(ethMetricObj.MetricID, ethMetricObj.TransactionUUID, ethMetricObj)
+				if errWhenUpdatingMetricToDB != nil {
+					logrus.Error("Error while updating metric details to the DB " + errWhenUpdatingMetricToDB.Error())
+					commons.JSONErrorReturn(w, r, errWhenUpdatingMetricToDB.Error(), http.StatusInternalServerError, "Error while updating metric details to the DB ")
+					return
+				}
+			}
+			logrus.Info("Error when generating BIN file, ERROR : " + errWhenGeneratingBIN.Error())
+			commons.JSONErrorReturn(w, r, errWhenGeneratingBIN.Error(), http.StatusInternalServerError, "Error when generating BIN file, ERROR : ")
+			return
+		}
+		ethMetricObj.ABIstring = binString
 
 	} else {
 		logrus.Info("Invalid metric status " + status)
