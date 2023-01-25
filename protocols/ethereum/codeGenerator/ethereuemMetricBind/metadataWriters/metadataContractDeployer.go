877f9885ac8866344535eb2541db497f2a74773b
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2023-01-25 11:44:51 +0530

    TRAC-7222 Metadata parent method structure

---
+++ protocols/ethereum/codeGenerator/ethereuemMetricBind/metadataWriters/metadataContractDeployer.go
@@ -0,0 +1,20 @@
+package metadatawriters
+
+import "github.com/dileepaj/tracified-gateway/model"
+
+//! Relevant map ID will be checked on the route handler to see whether the metric contract is already deployed or not
+func MetricMetadataContractDeployer(element model.MetricMetadataReq, metricMapID string) {
+
+	//TODO generate the contract
+
+	//TODO build the template
+
+	//TODO write into the solidity file
+
+	//TODO generate ABI
+
+	//TODO generate the BIN
+
+	//TODO add the request to the queue
+
+}
