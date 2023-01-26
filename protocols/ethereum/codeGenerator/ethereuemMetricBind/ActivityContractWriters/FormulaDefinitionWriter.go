b07554c9e03ce7aeb0d7c6a728e6c1b9e58b409c
T Herath <hkalani00@gmail.com>
2023-01-26 11:33:07 +0530

    TRAC-7225 formula declaration method

---
+++ protocols/ethereum/codeGenerator/ethereuemMetricBind/ActivityContractWriters/FormulaDefinitionWriter.go
@@ -0,0 +1,42 @@
+package activitywriters
+
+import (
+	"encoding/base64"
+	"strconv"
+	"strings"
+
+	"github.com/dileepaj/tracified-gateway/model"
+)
+
+// Get the contract address of the formula
+// Get the number of values in the formula
+// Get the value IDs of the formula
+// Convert the activity name to base64
+// Create the formula definition code and return it
+
+func GetFormulaDefinitionCode(element model.MetricDataBindActivityRequest) (string, error) {
+	// Get the contract address of the formula 
+	contractAddress, errInGettingContractAddress := GetFormulaContractAddress(element.MetricFormula.ID)
+	if errInGettingContractAddress != nil {
+		return "", errInGettingContractAddress
+	}
+
+	// Getting value IDs string
+	valueIDs := []string{} 
+	for _, value := range element.MetricFormula.Formula {
+		valueIDs = append(valueIDs, value.ID)
+	}
+	valueIDsString = strings.Join(valueIDs, ", ")
+
+	formulaDefinitionStart := `Formula private formula = Formula(`
+	formulaIDCode := `"` + element.MetricFormula.ID + `", `
+	contractAddressCode := `"` + contractAddress + `", `
+	noOfValuesCode := strconv.Itoa(len(element.MetricFormula.Formula)) + `, `
+	activityIDCode := `"` + element.ID + `", `
+	activityNameCode := `"` + base64.StdEncoding.EncodeToString([]byte(element.Name)) + `", `
+	valueIDsCode := `"` + valueIDsString + `"`
+	formulaDefinitionEnd := `);`
+	formulaDefinitionCode := formulaDefinitionStart + formulaIDCode + contractAddressCode + noOfValuesCode + activityIDCode + activityNameCode + valueIDsCode + formulaDefinitionEnd
+
+	return formulaDefinitionCode, nil
+}
