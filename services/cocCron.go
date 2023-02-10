50ef9eac3d42a35eea3fa477669aaae0cc486ab4
Bhawantha Pasan Shanuka <bpsshanuka@gmail.com>
2023-02-10 13:47:31 +0530

    TRAC-5923 change logs

--- services/cocCron.go
+++ services/cocCron.go
@@ -16,7 +16,7 @@ import (
 
 // CheckCOCStatus
 func CheckCOCStatus() {
-	if (commons.GoDotEnvVariable("LOGSTYPE")=="DEBUG"){
+	if commons.GoDotEnvVariable("LOGSTYPE") == "DEBUG" {
 	log.Debug("----------------------------------- CheckCOCStatus -------------------------------------")
 	}
 	object := dao.Connection{}
@@ -31,7 +31,7 @@ func CheckCOCStatus() {
 				var txe xdr.Transaction
 				err := xdr.SafeUnmarshalBase64(result[i].AcceptXdr, &txe)
 				if err != nil {
-					if (commons.GoDotEnvVariable("LOGSTYPE")=="CRON"){
+					if commons.GoDotEnvVariable("LOGSTYPE") == "DEBUG"  {
 						log.Error("Error @SafeUnmarshalBase64 @CheckCOCStatus" + err.Error())
 					}
 				}
@@ -39,8 +39,8 @@ func CheckCOCStatus() {
 					// result[i].Status="expired"
 					err1:=object.UpdateCOC(result[i],temp)
 					if err1!= nil{
-						if (commons.GoDotEnvVariable("LOGSTYPE")=="CRON"){
-						log.Error("Error @UpdateCOC" + err1.Error())
+						if commons.GoDotEnvVariable("LOGSTYPE") == "DEBUG"  {
+							log.Error("Error @UpdateCOC" + err1.Error())
 						}
 					}
 				}else{
@@ -51,7 +51,9 @@ func CheckCOCStatus() {
 		}
 		return nil
 	}).Catch(func(error error) error {
-		log.Error("Error @GetCOCbyStatus " + error.Error())
+		if commons.GoDotEnvVariable("LOGSTYPE") == "DEBUG"  {
+			log.Error("Error @GetCOCbyStatus " + error.Error())
+		}
 		return error
 	})
 	p.Await()
@@ -71,7 +73,7 @@ func CheckCOCExpired() {
 				var txe xdr.Transaction
 				err := xdr.SafeUnmarshalBase64(result[i].AcceptXdr, &txe)
 				if err != nil {
-					if (commons.GoDotEnvVariable("LOGSTYPE")=="CRON"){
+					if commons.GoDotEnvVariable("LOGSTYPE") == "DEBUG"  {
 						log.Error("Error @SafeUnmarshalBase64 @CheckCOCExpired " + err.Error())
 					}
 				}
@@ -79,20 +81,17 @@ func CheckCOCExpired() {
 					// result[i].Status="expired"
 					err1:=object.UpdateCOC(result[i],temp)
 					if err1!= nil{
-						if (commons.GoDotEnvVariable("LOGSTYPE")=="CRON"){
-						log.Error("Error @UpdateCOC @CheckCOCExpired "+err1.Error())
+						if commons.GoDotEnvVariable("LOGSTYPE") == "DEBUG"  {
+							log.Error("Error @UpdateCOC @CheckCOCExpired "+err1.Error())
 						}
 					}
-					// fmt.Println("Expired")
 				}else{
-					// fmt.Println("Not Expired")
 				}
-
 			}
 		}
 		return nil
 	}).Catch(func(error error) error {
-		if (commons.GoDotEnvVariable("LOGSTYPE")=="CRON"){
+		if commons.GoDotEnvVariable("LOGSTYPE") == "DEBUG"  {
 			log.Error("Error @GetCOCbyStatus @CheckCOCExpired "+error.Error())
 		}
 		return error
