11bdc10df44037372a9a92cf5afef9a2069a9dfd
T Herath <hkalani00@gmail.com>
2023-02-16 10:29:13 +0530

    TRAC-7448 abstract classes for getters and setters

---
+++ services/ethereumServices/setterCaller/setterCaller.go
@@ -0,0 +1,7 @@
+package settercaller
+
+// Returns the hash of the transaction and error if any
+func EthereumSetterCaller(methodName string, abi string, bin string, contractAddress string) (string, error) {
+
+	return "", nil
+}
