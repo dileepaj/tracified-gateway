0f37ed1546c01fb8019e68fd76c8bd0934aad27b
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2023-03-02 12:50:30 +0530

    TRAC-7656 initial structure for redeploy

---
+++ services/ethereumServices/contractDeployer/failedContractDeployer.go
@@ -0,0 +1,9 @@
+package contractdeployer
+
+import "github.com/dileepaj/tracified-gateway/model"
+
+func RedeployFailedContracts(failedContract model.PendingContracts) {
+	//TODO
+	//Get the ABI and BIN from DB for the needed contract type and identifier
+	//
+}
