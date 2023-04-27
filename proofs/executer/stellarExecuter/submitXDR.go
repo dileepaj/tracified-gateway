dc4b86b8f57b9e2f5c978298c6c879355851e39f
Bhawantha Pasan Shanuka <bpsshanuka@gmail.com>
2023-04-27 07:43:27 +0530

    TRAC-8226 resubmitting XDR to stellar for 504

--- proofs/executer/stellarExecuter/submitXDR.go
+++ proofs/executer/stellarExecuter/submitXDR.go
@@ -1,15 +1,15 @@
 package stellarExecuter
 
 import (
+	"net/http"
+	"strconv"
+	"time"
+
 	"github.com/dileepaj/tracified-gateway/commons"
 	log "github.com/sirupsen/logrus"
 
-	"net/http"
-	"time"
-
 	"github.com/stellar/go/clients/horizonclient"
 
-	// "github.com/dileepaj/tracified-gateway/api/apiModel"
 	"github.com/dileepaj/tracified-gateway/model"
 )
 
@@ -27,46 +27,35 @@ func (cd *ConcreteSubmitXDR) SubmitXDR(tType string) model.SubmitXDRResponse {
 	log.Debug("=========================== submitXDR.go SubmitXDR =============================")
 	horizonClient := commons.GetHorizonClient()
 	var response model.SubmitXDRResponse
-	//s := time.Now().UTC().String()
-
-	//f, err := os.OpenFile("GatewayLogs"+s[:10], os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	//if err != nil {
-	//	log.Fatalf("error opening file: %v", err)
-	//}
-	//defer f.Close()
-
-	//log.SetOutput(f)
-	// log.Println("This is a test log entry")
 	resp, err := horizonClient.SubmitTransactionXDR(cd.XDR)
-	if err != nil{
+	if err != nil {
 		log.Error("Error while SubmitTransaction to stellar test net " + err.Error())
 		error1 := err.(*horizonclient.Error)
-		log.Error(error1.Problem.Detail)
+		log.Error(" Error Message ", error1.Problem.Detail)
+		log.Error(time.Now().UTC().String() + "- Error code: " + strconv.Itoa(error1.Response.StatusCode))
+		log.Error(time.Now().UTC().String() + "- Error code: " + strconv.Itoa(error1.Problem.Status))
+		if error1.Response.StatusCode == 504 && error1.Problem.Status == 504 {
+			log.Info("Resubmitting transaction", cd.XDR)
+			display := ConcreteSubmitXDR{XDR: cd.XDR}
+			display.SubmitXDR(tType)
+		}
 		TC, err := error1.ResultCodes()
 		if err != nil {
 			log.Error("Error while getting ResultCodes from horizon.Error")
 		}
-
-    if TC != nil{
+		if TC != nil {
 			response.Error.Message = TC.TransactionCode
 		}
-		log.Error(time.Now().UTC().String() + "- TXNType:" + tType + " " + response.Error.Message + "  ", cd.XDR)
-		log.Error(time.Now().UTC().String() + "- TXNType:" + tType + " " + response.Error.Message + "  ")
-
+		log.Error(time.Now().UTC().String()+"- TXNType:"+tType+" "+response.Error.Message+"  ", cd.XDR)
+		log.Error(time.Now().UTC().String() + "- Error Message: " + response.Error.Message)
 		response.Error.Code = http.StatusBadRequest
-		// response.Error.Message = err.Error()
 		return response
 	}
-
-	// fmt.Println("Successful Transaction:")
 	log.Info("Ledger:", resp.Ledger)
 	log.Info(time.Now().UTC().String() + "- TXNType:" + tType + " Hash:" + resp.Hash)
-
 	response.Error.Code = http.StatusOK
 	response.Error.Message = "Transaction performed in the blockchain."
 	log.Info("Transaction performed in the blockchain.  " + resp.Hash)
 	response.TXNID = resp.Hash
-
 	return response
-
 }
