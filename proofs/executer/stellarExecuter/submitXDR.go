5f01636f8b22239a3cf78539ce56a8e4eb83e4fc
Keleigh123 <luciaberenger@gmail.com>
2023-04-27 12:15:10 +0530

    TRAC-8290-Adding a counter and filter on error message to submitXDR function

--- proofs/executer/stellarExecuter/submitXDR.go
+++ proofs/executer/stellarExecuter/submitXDR.go
@@ -23,6 +23,8 @@ type ConcreteSubmitXDR struct {
 @desc - Submits the XDR to stellar horizon api and returns the TXN hash.
 @params - XDR
 */
+var count int = 0
+
 func (cd *ConcreteSubmitXDR) SubmitXDR(tType string) model.SubmitXDRResponse {
 	log.Debug("=========================== submitXDR.go SubmitXDR =============================")
 	horizonClient := commons.GetHorizonClient()
@@ -34,28 +36,43 @@ func (cd *ConcreteSubmitXDR) SubmitXDR(tType string) model.SubmitXDRResponse {
 		log.Error(" Error Message ", error1.Problem.Detail)
 		log.Error(time.Now().UTC().String() + "- Error code: " + strconv.Itoa(error1.Response.StatusCode))
 		log.Error(time.Now().UTC().String() + "- Error code: " + strconv.Itoa(error1.Problem.Status))
-		if error1.Response.StatusCode == 504 && error1.Problem.Status == 504 {
-			log.Info("Resubmitting transaction", cd.XDR)
-			display := ConcreteSubmitXDR{XDR: cd.XDR}
-			display.SubmitXDR(tType)
-		}
-		TC, err := error1.ResultCodes()
+		RC, err := error1.ResultCodes()
 		if err != nil {
 			log.Error("Error while getting ResultCodes from horizon.Error")
 		}
-		if TC != nil {
-			response.Error.Message = TC.TransactionCode
+		if RC != nil {
+			response.Error.Message = RC.TransactionCode
 		}
-		log.Error(time.Now().UTC().String()+"- TXNType:"+tType+" "+response.Error.Message+"  ", cd.XDR)
-		log.Error(time.Now().UTC().String() + "- Error Message: " + response.Error.Message)
-		response.Error.Code = http.StatusBadRequest
+		log.Error(" Error Response ", response.Error.Message)
+		if (error1.Response.StatusCode == 504 && error1.Problem.Status == 504 && count < 10) || (response.Error.Message == "tx_insufficient_fee" && count < 10) {
+			log.Info("Resubmitting transaction", cd.XDR)
+			display := ConcreteSubmitXDR{XDR: cd.XDR}
+			count = count + 1
+			display.SubmitXDR(tType)
+		} else if (error1.Response.StatusCode == 504 && error1.Problem.Status == 504 && count >= 10) || (response.Error.Message == "tx_insufficient_fee" && count >= 10) {
+			log.Error("Error while submitting xdr to blockchain after retrying 10 times")
+			return response
+		} else {
+			TC, err := error1.ResultCodes()
+			if err != nil {
+				log.Error("Error while getting ResultCodes from horizon.Error")
+			}
+			if TC != nil {
+				response.Error.Message = TC.TransactionCode
+			}
+			log.Error(time.Now().UTC().String()+"- TXNType:"+tType+" "+response.Error.Message+"  ", cd.XDR)
+			log.Error(time.Now().UTC().String() + "- Error Message: " + response.Error.Message)
+			response.Error.Code = http.StatusBadRequest
+			return response
+		}
+	} else {
+		log.Info("Ledger:", resp.Ledger)
+		log.Info(time.Now().UTC().String() + "- TXNType:" + tType + " Hash:" + resp.Hash)
+		response.Error.Code = http.StatusOK
+		response.Error.Message = "Transaction performed in the blockchain."
+		log.Info("Transaction performed in the blockchain.  " + resp.Hash)
+		response.TXNID = resp.Hash
 		return response
 	}
-	log.Info("Ledger:", resp.Ledger)
-	log.Info(time.Now().UTC().String() + "- TXNType:" + tType + " Hash:" + resp.Hash)
-	response.Error.Code = http.StatusOK
-	response.Error.Message = "Transaction performed in the blockchain."
-	log.Info("Transaction performed in the blockchain.  " + resp.Hash)
-	response.TXNID = resp.Hash
 	return response
 }
