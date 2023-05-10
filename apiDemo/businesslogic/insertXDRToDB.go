37421aec111cc972b9ea9ca278b31c143686d492
Bhawantha Pasan Shanuka <bpsshanuka@gmail.com>
2023-05-10 15:34:26 +0530

    TRAC-8403  sanity check and ignore passing payload directly to the business logic - submit genesis

--- apiDemo/businesslogic/insertXDRToDB.go
+++ apiDemo/businesslogic/insertXDRToDB.go
@@ -19,8 +19,8 @@ type AbstractXDR struct {
 }
 
 func (AP *AbstractXDR) GenesisAndDataXDRToCron() (error, int, string) {
-	var task []model.Task                                               // array to decide whether the actions are done
-	task = append(task, model.Task{Identifier: "Task 1", Status: true}) // starting with a true for bipass
+	var task []model.Task                                         // array to decide whether the actions are done
+	task = append(task, model.Task{Identifier: "", Status: true}) // starting with a true for bipass
 	connect := dao.Connection{}
 	for i, TxnBody := range AP.TxnBody {
 		var txe xdr.Transaction
@@ -42,7 +42,7 @@ func (AP *AbstractXDR) GenesisAndDataXDRToCron() (error, int, string) {
 		err2, err2Code := connect.InsertTempOrphan(AP.TxnBody[i])
 		if err2 != nil {
 			// fmt.Println("Error @ InsertSpecialToTempOrphan @SubmitSpecial " + err2.Error())
-			task = append(task, model.Task{Identifier: "Task 1", Status: true})
+			task = append(task, model.Task{Identifier: AP.TxnBody[i].Identifier, Status: false})
 			// w.WriteHeader(http.StatusBadRequest)
 			// response := apiModel.SubmitXDRSuccess{
 			// 	Status: "Index[" + strconv.Itoa(i) + "] TXN: Scheduling Failed!",
