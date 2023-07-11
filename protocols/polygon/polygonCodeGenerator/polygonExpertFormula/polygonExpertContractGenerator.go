d6b18aa7296c9a1ce5e23cbdf821fbcdff669324
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2023-07-11 12:16:27 +0530

    TRAC-8926 Initial expert formula generator

---
+++ protocols/polygon/polygonCodeGenerator/polygonExpertFormula/polygonExpertContractGenerator.go
@@ -0,0 +1,11 @@
+package polygonexpertformula
+
+import (
+	"net/http"
+
+	"github.com/dileepaj/tracified-gateway/model"
+)
+
+func PolygonExpertFormulaContractGenerator(w http.ResponseWriter, r *http.Request, formulaJSON model.FormulaBuildingRequest, fieldCount int) {
+
+}
