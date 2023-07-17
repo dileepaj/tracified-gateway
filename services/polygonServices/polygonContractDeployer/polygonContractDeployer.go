370e4915726ed21f0414caaf18b064d8fdbebd0c
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2023-07-17 13:57:42 +0530

    TRAC-8983 ABI and BIN loader

--- services/polygonServices/polygonContractDeployer/polygonContractDeployer.go
+++ services/polygonServices/polygonContractDeployer/polygonContractDeployer.go
@@ -2,12 +2,14 @@ package polygoncontractdeployer
 
 import (
 	"errors"
+	"math/big"
 
 	"github.com/dileepaj/tracified-gateway/constants"
 	"github.com/dileepaj/tracified-gateway/dao"
 	"github.com/dileepaj/tracified-gateway/model"
 	generalservices "github.com/dileepaj/tracified-gateway/services/ethereumServices/generalServices"
 	"github.com/dileepaj/tracified-gateway/utilities"
+	"github.com/dileepaj/tracified-gateway/vendor/github.com/ethereum/go-ethereum/accounts/abi/bind"
 )
 
 func PolygonContractDeployer(bin string, abi string, contractIdentifier string, contractType string, otherParams []any) (string, string, string, error) {
@@ -38,8 +40,24 @@ func PolygonContractDeployer(bin string, abi string, contractIdentifier string, 
 	}
 
 	logger.LogWriter(client, constants.INFO)
-	logger.LogWriter(privateKey, constants.INFO)
 	logger.LogWriter(fromAddress, constants.INFO)
+
+	ContractBIN, parsed, errWhenLoadingParsedABIAndBIN := generalservices.LoadContractBinAndParsedAbi(bin, abi)
+	if errWhenLoadingParsedABIAndBIN != nil {
+		logger.LogWriter("Error when loading ContractBIN and Parsed ABI : "+errWhenLoadingParsedABIAndBIN.Error(), constants.ERROR)
+		return contractAddress, transactionHash, transactionCost, errors.New("Error when loading ContractBIN and Parsed ABI : " + errWhenLoadingParsedABIAndBIN.Error())
+	}
+
+	logger.LogWriter(ContractBIN, constants.INFO)
+
+	if parsed == nil {
+		logger.LogWriter("GetABI returned nil", constants.ERROR)
+		return contractAddress, transactionHash, transactionCost, errors.New("Error when getting ABI string , ERROR : GetAbi() returned nil")
+	}
+
+	//create the keyed transactor
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Value = big.NewInt(0) // in wei
 
 	return contractAddress, transactionHash, transactionCost, nil
 }
